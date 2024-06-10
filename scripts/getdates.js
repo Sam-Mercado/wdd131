be17bc728a483f220bc769976ba12e750d3b179f
Samuel Mercado <smercadoperez67@gmail.com>
2024-06-10 14:41:23 -0400

    Week1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,13 @@
-const lastModified= document.querySelector("#lastModified");
 
-LastModif = new Date(document.lastModified)
+
+const year = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+year.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>&copy; Samuel Mercado - Bolivia,Cochabamba`;
+
+const lastModifiedDate = new Date(document.lastModified);
+// Formats date to a readable string
+const formattedDate = lastModifiedDate.toLocaleString(); 
+
+lastModified.innerHTML = `Last modified: ${formattedDate}`;
